f54648d8b11426a84b9beab4a93428cb794c1a2b
VirlanIon <virlan.ion.petru@gmail.com>
2016-03-31 13:11:25 +0400

    Varianta initiala a proiectului(vor urma verificari)

---
+++ Laborator 3/Source code/Calcutator/Calcutator/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcutator
+{
+     public partial class Form1 : Form
+     {
+          Double resultValue = 0;
+          String operationPerfomed = "";
+          bool isOperationPerfomed = false;
+          public Form1()
+          {
+               InitializeComponent();
+          }
+
+          private void Form1_Load(object sender, EventArgs e)
+          {
+
+          }
+
+          private void button_click(object sender, EventArgs e)
+          {
+               if ((textBox_Result.Text == "0") || (isOperationPerfomed))
+                    textBox_Result.Clear();
+
+               isOperationPerfomed = false;
+               Button button = (Button)sender;
+               textBox_Result.Text = textBox_Result.Text + button.Text;
+          }
+
+          private void operator_click(object sender, EventArgs e)
+          {
+               Button button = (Button)sender;
+               operationPerfomed = button.Text;
+               resultValue = Double.Parse(textBox_Result.Text);
+               labelCurrentOperation.Text = resultValue + " " + operationPerfomed;
+               isOperationPerfomed = true;
+          }
+
+          private void button5_Click(object sender, EventArgs e)
+          {
+               textBox_Result.Text = "0";
+          }
+
+          private void button10_Click(object sender, EventArgs e)
+          {
+               textBox_Result.Text = "0";
+               resultValue = 0;
+          }
+
+          private void button20_Click(object sender, EventArgs e)
+          {
+               switch (operationPerfomed)
+               {
+                    case "+":
+                         textBox_Result.Text = (resultValue + Double.Parse(textBox_Result.Text)).ToString();
+                         break;
+                    case "-":
+                         textBox_Result.Text = (resultValue - Double.Parse(textBox_Result.Text)).ToString();
+                         break;
+                    case "*":
+                         textBox_Result.Text = (resultValue * Double.Parse(textBox_Result.Text)).ToString();
+                         break;
+                    case "/":
+                         textBox_Result.Text = (resultValue / Double.Parse(textBox_Result.Text)).ToString();
+                         break;
+                    default:
+                         break;
+               }
+          }
+     }
+}
